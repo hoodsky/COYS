9baf68fd25332b574ffa435968cae0467e6d525a
hoodsky <Hoodsky.ks@gmail.com>
2022-05-28 12:30:43 +0300

    4

--- javascript/script.js
+++ javascript/script.js
@@ -33,6 +33,93 @@ function includeHTML() {
 // скрипт для вставки шапки и подвала на страницу
 includeHTML();
 
+// предзагрузка страницы
+
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+
+    window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 500);
+}
+
+//слайдер
+
+const slides = document.querySelectorAll('.slide');
+const carouselTrack = document.querySelector('.slider__slides');
+const dotsList = document.querySelector('.slider__dots');
+
+let activeSlideIndex = 0;
+
+const updateInd = () => {
+    let dots = document.querySelectorAll('.dot');
+    dots.forEach((el, idx) => {
+        el.classList.remove('active');
+        if (idx === activeSlideIndex) {
+            el.classList.add('active');
+        }
+    })
+}
+
+const moveSlide = dir => {
+    let imgWidth = slides[activeSlideIndex].clientWidth;
+    if (dir === "prev") {
+        if (activeSlideIndex > 0) {
+            activeSlideIndex--;
+        } else {
+            activeSlideIndex = slides.length - 1;
+        }
+    } else if (dir === "next") {
+        if (activeSlideIndex < slides.length - 1) {
+            activeSlideIndex++;
+        } else {
+            activeSlideIndex = 0;
+        }
+    }
+    carouselTrack.style.transform = `translateX(-${activeSlideIndex * imgWidth}px)`;
+    updateInd();
+}
+
+const moveSlides = idx => {
+    let diff = idx - activeSlideIndex;
+    if (diff >= 0) {
+        for (let i = 0; i < diff; i++) {
+            moveSlide("next");
+        }
+    } else {
+        diff *= -1;
+        for (let i = 0; i < diff; i++) {
+            moveSlide("prev");
+        }
+    }
+}
+
+const generateInd = () => {
+    for (let i = 0; i < slides.length; i++) {
+        let newItem = document.createElement('li');
+        newItem.classList.add('dot');
+        newItem.setAttribute('data-index', i);
+        dotsList.appendChild(newItem);
+    }
+    updateInd();
+}
+
+dotsList.addEventListener('click', e => {
+    let target = e.target;
+    if (target.classList.contains('dot')) {
+        console.log(target.dataset.index);
+        moveSlides(target.dataset.index);
+    }
+})
+
+generateInd();
+
+
+
+
+
+
 //проверка ввода текста для активации кнопки
 
 function checkInput() {
@@ -46,13 +133,3 @@ function checkInput() {
 
 checkInput();
 
-// предзагрузка страницы
-
-window.onload = function () {
-    document.body.classList.add('loaded_hiding');
-
-    window.setTimeout(function () {
-        document.body.classList.add('loaded');
-        document.body.classList.remove('loaded_hiding');
-    }, 500);
-}
